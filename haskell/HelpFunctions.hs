8f6e4836a6b34f317a05141c59c96d4149e5f534
Runar <runarkristins@gmail.com>
2014-03-30 12:32:01 +0200

    Working on constraints

--- haskell/HelpFunctions.hs
+++ haskell/HelpFunctions.hs
@@ -18,6 +18,35 @@ type Plan = [String]
 data PDDL = Primative Id Id
 --(ontop a b), (ontop b floor-n)
 
+data Form = Brick | Plank | Table | Box | Pyramid | Ball | Floor
+			deriving (Show)
+-- Maybe use Eq to add constraints ? for example, Brick > Ball = True
+-- So Brick can suport Ball
+data Size = Large | Small
+			deriving (Eq,Ord, Show)
+type Color = String
+
+data Object = Obj Id Form Size Color
+
+instance Show Object where
+	show (Obj i f s c) =  "[Obj " ++ i ++ " " ++ show f  ++ " " ++ show s ++ " " ++ c ++ "]"
+
+listOfObjects :: [Object]
+listOfObjects = [Obj "a" Brick   Large "green"
+				,Obj "b" Brick   Small "white"
+				,Obj "c" Plank   Large "red"
+				,Obj "d" Plank   Small "green"
+				,Obj "e" Ball    Large "white"
+				,Obj "f" Ball    Small "black"
+				,Obj "g" Table   Large "blue"
+				,Obj "h" Table   Small "red"
+				,Obj "i" Pyramid Large "yellow"
+				,Obj "j" Pyramid Small "red"
+				,Obj "k" Box     Large "yellow"
+				,Obj "l" Box     Small "red"
+				,Obj "m" Box     Large "blue"
+				]
+
 instance Show PDDL where
 	show (Primative i1 i2) = "ontop " ++ i1 ++ " "  ++ i2
 
@@ -38,22 +67,61 @@ take' i w = (getId fstItem ,newWorld)
           
           
 
---kann ekki ad pattern matcha tetta er madur ad fara ad nota primative?
---þurfum eitthvað sem finnur efsta objectið, take nema ignore world >ld
+-- Returns the first id of a PDDL          
 getId :: PDDL -> Id
-getId (Primative a b) = a             
+getId (Primative a _) = a           
+
+-- Returns object from id
+getObjId :: Id -> Object
+getObjId id = getO id listOfObjects
+        where 
+            getO id ((Obj i f s c):objs) | id == i = Obj i f s c
+                                         | otherwise = getO id objs 
+ 
+okMove :: Object -> Object -> Bool
+okMove _                        (Obj _ Ball _ _ )        = False    -- Balls can't support anything.
+okMove (Obj i1 Plank s1 c1)     (Obj i2 Box s2 c2)       = s1 < s2 -- Boxes cannot contain  planks  of the same size.
+okMove (Obj i1 Pyramid s1 c1)   (Obj i2 Box s2 c2)       = s1 < s2 -- Boxes cannot contain pyramids of the same size.
+okMove (Obj i1 Box Large c1)    (Obj i2 Brick Large c2)  = True     -- but large boxes can also be supported by large bricks.
+okMove (Obj i1 Box s1 c1)       (Obj i2 Table s2 c2)     = s1 == s2 -- Boxes can only be supported by tables of the same size.
+okMove (Obj i1 Box s1 c1)       (Obj i2 Plank s2 c2)     = s1 == s2 -- Boxes can only be supported by planks of the same size.
+okMove (Obj i1 Ball s1 c1)      (Obj i2 Floor s2 c2)     = True     -- Balls must be on the floor, otherwise they roll away. 
+okMove (Obj i1 Ball s1 c1)      (Obj i2 Box s2 c2)       = s1 <= s2 -- Balls must be in a box,     otherwise they roll away. 
+okMove (Obj _ _ s1 _)           (Obj _ _ s2 _)           = s1 < s2  -- Small objects cannot support large objects.
+okMove _                        _                        = False    -- other
 
 
---Add object to the top of tack i and return the new world
+
+{-
+
+    The floor can support any number of objects.
+    All objects must be supported by something.
+    The arm can only hold one object at the time.
+    The arm can only pick up free objects.
+    Objects are “in” boxes, but “on” other objects.
+    -Balls must be in boxes or on the floor, otherwise they roll away.
+    -Balls cannot support anything.
+    -Small objects cannot support large objects.
+    -Boxes cannot contain pyramids or planks of the same size.
+    -Boxes can only be supported by tables or planks of the same size, but large boxes can also be supported by large bricks.
+
+-}
+
+
+--2 Add object to the top of stack i and return the new world
 drop' :: Int -> PDDLWorld -> Id -> PDDLWorld
 drop' i  w  id = newWorld
         where
           stack = (w !! i)
           fstItemid = case stack of 
                       [] -> Primative id ("floor-" ++ (show i))
-                      b  -> Primative id (getId (head stack)) 
+                      b  -> do
+                            let obj1 = getObjId id
+                            let obj2 = getObjId oldId
+                            if (okMove obj1  obj2) then Primative "ERROR" "ERROR" else (Primative id oldId) --Check if illegal move is about to be made
+          oldId = getId (head stack)
           (l,r) = splitAt i w
-          newWorld = l ++[(fstItemid : stack)] ++ tail r-- djofull ljott
+          newWorld = l ++[(fstItemid : stack)] ++ tail r
 
 
 -- An idea to set up the world in PDDL form
@@ -65,6 +133,9 @@ convertWorld n (c:cs) = (reverse (createPDDL n c)):convertWorld (n+1) cs
 				createPDDL k (x:xs) = 
 				 Primative x ("floor-" ++ (show k)) :[Primative (c !! (i+1)) (c !! i)
 				  | i <-[0..length xs -1]]
+				  
+convertObjects :: Objects -> [Object]
+convertObjects o = undefined
 
 ok :: Result a -> a
 ok (Ok res) = res

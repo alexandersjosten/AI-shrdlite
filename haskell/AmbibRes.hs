149b59fe7e9bbc2dbb275b69b1b7d23069655650
Behrouz Talebi <berrat2@gmail.com>
2014-04-08 19:45:22 +0200

    Changed alot of stuff, and made sure we can find where the ambiguity lies.

--- haskell/AmbibRes.hs
+++ haskell/AmbibRes.hs
@@ -2,30 +2,51 @@ module AmbigRes where
 
 import ShrdliteGrammar
 import HelpFunctions
+import Data.Maybe
 
+-- For testing only (will be removed later!)
+testAmbigA :: [PDDL]
+testAmbigA = [PDDL Ontop "a" "b", PDDL Ontop "a" "c", PDDL Ontop "b" "c"]
 
---Ambiguity for when an object is to be placed on top of several objects at once
-isAmbiguousOn :: [PDDL] -> Bool
-isAmbiguousOn [] = False
-isAmbiguousOn ((PDDL _ a _):xs) =
-  if null [y | y@(PDDL _ a' _) <- xs, a == a'] then
-    --We found no ambiguity for a, so we want to check the rest of the list
-    isAmbiguous xs
-  else
-    --We found an ambiguity so we want to point out which object it concerns.
-    --Ask the user where it wants to put the ambious object. TODO
-    True
+testAmbigB :: [PDDL]
+testAmbigB = [PDDL Ontop "a" "c", PDDL Ontop "b" "c", PDDL Ontop "c" "d"]
 
---Ambiguity for when an object is to be supporting many objects at once
-isAmbiguousUnder :: [PDDL] -> Bool
-isAmbiguousUnder [] = False
-isAmbiguousUnder ((PDDL _ _ a):xs) =
-  if null [y | y@(PDDL _ _ a') <- xs, a == a'] then
-    --We found no ambiguity for a, so we want to check the rest of the list
-    isAmbiguous xs
-  else
-    --We found an ambiguity so we want to point out which object it concerns.
-    --Ask the user where it wants to put the ambious object. TODO
-    True
+testAmbigNone :: [PDDL]
+testAmbigNone = [PDDL Ontop "a" "b", PDDL Ontop "b" "c", PDDL Ontop "c" "d"]
 
+---------------------------------------------------------------------------
 
+-- Check for any of the 2 ambiguous situation
+isAmbiguous :: [PDDL] -> Maybe String
+isAmbiguous [] = Nothing
+isAmbiguous xs =
+  case a of
+    Nothing      ->
+      case b of
+        Nothing      -> Nothing
+        Just (is, i) -> Nothing -- TODO
+    Just (is, i) -> Nothing -- TODO
+  where (PDDL _ id1 id2) = head xs
+        a                = checkForDups (id1, id2) (tail xs)
+        b                = checkForDups' (id1, id2) (tail xs)
+
+checkForDups :: (Id, Id) -> [PDDL] -> Maybe ([Id],Id)
+checkForDups _ [] = Nothing
+checkForDups (id1, id2) xs =
+  let ys =  [id2' | (PDDL _ id1' id2') <- xs, id1 == id1']
+  in if null ys then
+       let (PDDL _ id' id'') = head xs
+       in checkForDups (id',id'') (tail xs)
+     else
+       Just (id2:ys,id1)
+    
+
+checkForDups' :: (Id, Id) -> [PDDL] -> Maybe ([Id],Id)
+checkForDups' _          [] = Nothing
+checkForDups' (id1, id2) xs =
+  let ys =  [id1' | (PDDL _ id1' id2') <- xs, id2 == id2']
+  in if null ys then
+       let (PDDL _ id' id'') = head xs
+       in checkForDups (id', id'') (tail xs)
+     else
+       Just (id1:ys,id2)

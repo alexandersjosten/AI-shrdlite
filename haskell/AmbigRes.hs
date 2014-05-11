42f786ef8f09571f0bac08f07df9ccc9135b47e4
Behrouz Talebi <berrat2@gmail.com>
2014-05-11 23:17:01 +0200

    Fixed all the bugs in the Ambiguity Resolver, just the String part left TODO.

--- haskell/AmbigRes.hs
+++ haskell/AmbigRes.hs
@@ -4,59 +4,59 @@ import ShrdliteGrammar
 import HelpFunctions
 import Data.Maybe
 
-data Ambiguity = Ambiguity [Id] Id
-
--- For testing only (will be removed later!)
-testAmbigA :: [PDDL]
-testAmbigA = [PDDL Ontop "a" "b", PDDL Ontop "a" "c", PDDL Ontop "b" "c"]
-
-testAmbigB :: [PDDL]
-testAmbigB = [PDDL Ontop "a" "c", PDDL Ontop "b" "c", PDDL Ontop "c" "d"]
-
-testAmbigNone :: [PDDL]
-testAmbigNone = [PDDL Ontop "a" "b", PDDL Ontop "b" "c", PDDL Ontop "c" "d"]
+data Ambiguity = Ambiguity Bool Id [Id]
 
 ---------------------------------------------------------------------------
 
--- Check for any of the 2 ambiguous situation
 resolveAmbig :: [[PDDL]] -> Either Ambiguity [PDDL]
-resolveAmbig [] = error "impossibru"
-resolveAmbig [x] =  Right x
-resolveAmbig xs = undefined
+resolveAmbig []     = error "impossibru"
+resolveAmbig [x]    = Right x
+resolveAmbig (x:xs) =
+  case multiDst of
+    Nothing   -> case multiSrc of
+      Nothing   -> resolveAmbig xs
+      Just ambD -> Left ambD
+    Just ambS -> Left ambS
+    where
+      multiSrc = checkSrcDups x xs
+      multiDst = checkDstDups x xs
+
+
+checkDstDups :: [PDDL] -> [[PDDL]] -> Maybe Ambiguity
+checkDstDups [] [] = Nothing
+checkDstDups [] xs = checkDstDups (head xs) (tail xs)
+checkDstDups x xs  =
+  case dstList of
+    Nothing  -> checkDstDups (tail x) xs
+    Just amb -> Just amb
+    where
+      dstList = findAllDstDups (head x) xs
+    
+findAllDstDups :: PDDL -> [[PDDL]] -> Maybe Ambiguity
+findAllDstDups x []     = Nothing
+findAllDstDups (PDDL rel src dst) (x:xs) =
+  let ys =  [dst' | (PDDL _ src' dst') <- x, src == src']
+      in if null ys
+      then findAllDstDups (PDDL rel src dst) xs
+      else (Just (Ambiguity False src ys))
+
+checkSrcDups :: [PDDL] -> [[PDDL]] -> Maybe Ambiguity
+checkSrcDups [] [] = Nothing
+checkSrcDups [] xs = checkSrcDups (head xs) (tail xs)
+checkSrcDups x xs  =
+  case srcList of
+    Nothing  -> checkSrcDups (tail x) xs
+    Just amb -> Just amb
+    where
+        srcList = findAllSrcDups (head x) xs
+    
+findAllSrcDups :: PDDL -> [[PDDL]] -> Maybe Ambiguity
+findAllSrcDups x []     = Nothing
+findAllSrcDups (PDDL rel src dst) (x:xs) =
+  let ys =  [src' | (PDDL _ src' dst') <- x, dst == dst']
+      in if null ys
+      then findAllSrcDups (PDDL rel src dst) xs
+      else Just (Ambiguity True dst ys)
 
 buildChoices :: Ambiguity -> String
 buildChoices _ = "Hello!"
-
--- Check for any of the 2 ambiguous situation
-isAmbiguous :: [PDDL] -> Either Ambiguity [PDDL]
-isAmbiguous [] = error "IMPOSSIBRU"
-isAmbiguous xs =
-  case multiSrc of
-    Nothing -> case multiDst of
-        Nothing      -> Right xs
-        Just amb     -> Left amb -- TODO
-    Just amb -> Left amb -- TODO
-  where (PDDL _ id1 id2) = head xs
-        multiSrc         = checkSrcDups (id1, id2) (tail xs)
-        multiDst         = checkDstDups (id1, id2) (tail xs)
-
-checkSrcDups :: (Id, Id) -> [PDDL] -> Maybe Ambiguity
-checkSrcDups _ [] = Nothing
-checkSrcDups (id1, id2) xs =
-  let ys =  [id2' | (PDDL _ id1' id2') <- xs, id1 == id1']
-  in if null ys then
-       let (PDDL _ id' id'') = head xs
-       in checkSrcDups (id',id'') (tail xs)
-     else
-       Just $ Ambiguity (id2:ys) id1
-    
-
-checkDstDups :: (Id, Id) -> [PDDL] -> Maybe Ambiguity
-checkDstDups _          [] = Nothing
-checkDstDups (id1, id2) xs =
-  let ys =  [id1' | (PDDL _ id1' id2') <- xs, id2 == id2']
-  in if null ys then
-       let (PDDL _ id' id'') = head xs
-       in checkDstDups (id', id'') (tail xs)
-     else
-       Just $ Ambiguity (id1:ys) id2

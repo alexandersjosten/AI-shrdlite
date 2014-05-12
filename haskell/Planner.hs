b651f5d3fd41efd199e1c9339ba7cb6510201cbe
Asi <asbjornhp@gmail.com>
2014-05-12 12:17:21 +0000

    Talking bastard is alive

--- haskell/Planner.hs
+++ haskell/Planner.hs
@@ -31,7 +31,37 @@ amIAlone i w =  if or $ checkForm (Object s c f) objects
                 checkSize _ []                                     = []
                 checkSize (Object s1 c1 f1) ((Object s2 c2 f2):os) = (f1 == f2 && s1 == s2) : checkSize (Object s1 c1 f1) os
                 (Object s c f)  = getObjId i
-                objects = (convertToObjects w)  
+                objects = (convertToObjects w)
+                
+                
+startSentence :: Int -> String
+startSentence x | x < 3     =   "I can do this! It will only take " ++  show x ++ " moves. I start by moving the "
+                | x < 6     =   "This is almost a challenge, even Peter could do this! just " ++  show x ++ " moves. I start by moving the "
+                | otherwise =   "This is a hard one! It will take me all day to move " ++  show x ++ " objects! Ohhh well lets get started... I start by moving the "
+
+moveSentence :: Id -> String
+moveSentence id = case getObjId id of
+            (Object s c Box) -> "Ohhh this is heavy, I move "
+            (Object s c Ball) -> "Slippery shiiit, I move "
+            (Object s c Brick) -> "I don't want to move this one, ohh hell, I move " 
+            (Object s c Pyramid) ->"A pyramid, wtf, I move "
+            (Object s c Plank) ->"Fuff, it's hard to balance this one, I move  "
+            (Object s c Table) ->"Akwardddd,,, I move "
+
+startTB :: [Move] -> PDDLWorld -> Plan
+startTB [] _         = ["I found it "]
+startTB ((x,y):[]) w = do
+                 let (i,nw) = take' x w
+                 let stack =  (w !! y)
+                 let finaltext = case stack of
+                        [] -> " on the floor"
+                        ((PDDL Ontop a b):c) ->  " on the " ++  (amIAlone a (snd (take' y nw)))
+                 ["This is a easy one! I just move the " ++ amIAlone i nw ++  finaltext , " pick " ++  show x , "drop " ++ show y]
+startTB ((x,y):ms) w = do
+                 let (i,nw) = take' x w
+                 let nw'    = drop' y nw i
+                 [startSentence (length ((x,y):ms))  ++ amIAlone i nw ++ " then  " , " pick " ++  show x , "drop " ++ show y  ] ++ talkingBastard ms nw'
+
 
 talkingBastard :: [Move] -> PDDLWorld -> Plan
 talkingBastard [] _         = ["I found it "]
@@ -41,18 +71,18 @@ talkingBastard ((x,y):[]) w = do
                  let finaltext = case stack of
                         [] -> " on the floor"
                         ((PDDL Ontop a b):c) ->  " on the " ++  (amIAlone a (snd (take' y nw)))
-                 ["Finally I move the " ++ amIAlone i nw ++  finaltext , " pick " ++  show x , "drop " ++ show y]
+                 ["finally I move the " ++ amIAlone i nw ++  finaltext , " pick " ++  show x , "drop " ++ show y]
 talkingBastard ((x,y):ms) w = do
                  let (i,nw) = take' x w
                  let nw'    = drop' y nw i
-                 ["I move the " ++ amIAlone i nw , " pick " ++  show x , "drop " ++ show y] ++ talkingBastard ms nw'
+                 [moveSentence i ++ amIAlone i nw , " pick " ++  show x , "drop " ++ show y] ++ talkingBastard ms nw'
 
 -- Case for holding objects and goals when only taking up objects is next.
 solve :: World -> Id -> Id -> [PDDL] -> Plan
 solve world hold holding  ((PDDL t a b):goal)
                           | hold /= "no" = if holding == a && b=="" then [] else ["I drop " ++ amIAlone a (convertWorld world),"drop " ++ show holdMove ++ " "] ++ (solve newWorld "no" "-" goal')
-                          | b == ""        =  talkingBastard allMovesT (convertWorld world) ++ [" and now I'm holding the " ++ amIAlone a nw," pick " ++ show (fst $ findSAH a world)]
-                          | otherwise      =  talkingBastard allMoves (convertWorld world)
+                          | b == ""        =  startTB allMovesT (convertWorld world) ++ [" and now I'm holding the " ++ amIAlone a nw," pick " ++ show (fst $ findSAH a world)]
+                          | otherwise      =  startTB allMoves (convertWorld world)
                                             
     where
         allMoves   = fst $ runDfs goal' world
@@ -156,7 +186,6 @@ safeHead lst | lst == [] = ([],[])
 			 | otherwise = head lst
                                            
 
-                                
 -- Sort what stacks to start working on
 sortMoves :: (Int,Int) -> [Move] -> [Move]
 sortMoves (s1,s2) mv =  sortBy (sortGT s2) (sortBy (sortGT s1) mv)

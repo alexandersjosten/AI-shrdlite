07a06235e62c809471690b79024d0da0a704e293
Behrouz Talebi <berrat2@gmail.com>
2014-05-18 20:29:37 +0200

    Commented code

--- haskell/AmbigRes.hs
+++ haskell/AmbigRes.hs
@@ -5,66 +5,82 @@ import HelpFunctions
 import Data.Maybe
 import Data.Char
 
+-- The data types needed for modelling an ambiguity
 data AmbType = Source | Dest deriving (Show, Eq, Read)
 data Ambiguity = Ambiguity AmbType Id [Id] deriving (Show)
 
 ---------------------------------------------------------------------------
-
+--Top-level function that calls the helper functions
+--It calls the source search first and if resultless, the destination search
+--Precond: The list of lists of PDDLs can not be empty
+--Output: A singel goal in a list or an ambiguity
 resolveAmbig :: [[PDDL]] -> Either Ambiguity [PDDL]
-resolveAmbig []     = error "Not Possible"
+resolveAmbig []     = error "Not Possible" --Precond
 resolveAmbig [x]    = Right x
 resolveAmbig (x:xs) =
   case checkSrcDups x xs of
     Nothing -> case checkDstDups x xs of
-      Nothing -> case allSources (x:xs) of
+      Nothing -> case allSources (x:xs) of --Testing impossible worlds.
         Nothing -> Right x
-        Just a -> Left a
-      Just d  -> Left d
-    Just s  -> Left s
+        Just a -> Left a 
+      Just d  -> Left d --Destination amb.
+    Just s  -> Left s --Source amb.
 
+
+--For testing only in impossible cases
 allSources :: [[PDDL]] -> Maybe Ambiguity
 allSources [] = Nothing
 allSources xs = Just (Ambiguity Source "" ys)
   where ys = [ id | y <- xs, (PDDL _ id id2) <- y]
   
-    
+
+--Find all destination ambiguitys between target goal
+--in a list and the rest of the goals in the list        
 checkDstDups :: [PDDL] -> [[PDDL]] -> Maybe Ambiguity
 checkDstDups [] [] = Nothing
 checkDstDups x  [] = Nothing
 checkDstDups [] (y:ys) = checkDstDups y ys
 checkDstDups x xs  =
   case dstList of
-    Nothing  -> checkDstDups (tail x) xs
-    Just amb -> Just amb
+    Nothing  -> checkDstDups (tail x) xs --recursive step
+    Just amb -> Just amb --found amb.
     where
-        dstList = findAllDstDups (head x) xs
-        
+        dstList = findAllDstDups (head x) xs --helper function call
+
+--Function for comparing one PDDL with the rest of the PDDLs
+--in destination ambiguity
 findAllDstDups :: PDDL -> [[PDDL]] -> Maybe Ambiguity
 findAllDstDups x []     = Nothing
 findAllDstDups x xs =
-  let ys = concatMap (dstDups x) xs
+  let ys = concatMap (dstDups x) xs --append each list of id's that is generated
       in if null ys
       then Nothing
       else (Just (Ambiguity Dest src (dst:ys)))
            where
              (PDDL _ src dst) = x
-
+             
+--Compare PDDLs to see if the source matches but destination does not
+--Output is the list of all destination ids that did not match  
 dstDups :: PDDL -> [PDDL] -> [Id]
 dstDups x [] = []
 dstDups (PDDL rel src dst) xs =
   [dst' | (PDDL rel' src' dst') <- xs, src == src']
 
+--Find all source ambiguitys between target goal
+--in a list and the rest of the goals in the list   
 checkSrcDups :: [PDDL] -> [[PDDL]] -> Maybe Ambiguity
 checkSrcDups [] [] = Nothing
 checkSrcDups x  [] = Nothing
 checkSrcDups [] (y:ys) = checkSrcDups y ys
 checkSrcDups x xs  =
   case srcList of
-    Nothing  -> checkSrcDups (tail x) xs
-    Just amb -> Just amb
+    Nothing  -> checkSrcDups (tail x) xs --recursive step
+    Just amb -> Just amb --found amb.
     where
-        srcList = findAllSrcDups (head x) xs
-    
+        srcList = findAllSrcDups (head x) xs --helper function call
+
+--Function for comparing one PDDL with the rest of the PDDLs
+--in source ambiguity
 findAllSrcDups :: PDDL -> [[PDDL]] -> Maybe Ambiguity
 findAllSrcDups x []     = Nothing
 findAllSrcDups x xs =
@@ -74,11 +90,14 @@ findAllSrcDups x xs =
       else (Just (Ambiguity Source dst (src:ys)))
            where (PDDL _ src dst) = x
 
+--Compare PDDLs to see if the destination matches but source does not
+--Output is the list of all sources ids that did not match                 
 srcDups :: PDDL -> [PDDL] -> [Id]
 srcDups x [] = []
 srcDups (PDDL rel src dst) xs =
   [src' | (PDDL rel' src' dst') <- xs, dst == dst']
-  
+
+--Main string choice function
 buildChoices :: Ambiguity -> String
 buildChoices (Ambiguity isDst id listId)
   | length listId > 5 = "There is "
@@ -86,7 +105,8 @@ buildChoices (Ambiguity isDst id listId)
                          ++ " objects you could be refering to, be more specific!"
   | otherwise         = "Ambiguity error! Specify by entering a single number: "
                         ++ printObjects listId 1
-  
+
+--String representation of a list of objects, int used to number the list
 printObjects :: [Id] -> Int -> String
 printObjects [] _ = ""
 printObjects ("floor":xs) n = "(" ++ show n ++ ").The floor"
--- haskell/Shrdlite.hs
+++ haskell/Shrdlite.hs
@@ -27,9 +27,10 @@ main = do
 jsonMain :: JSObject JSValue -> JSValue
 jsonMain jsinput =
   let amb = (valFromObj "state" jsinput) :: Result String in
-  case amb of
+  case amb of --Find out if this a new utterance or clarifying state
     Ok prev | prev /= "" -> makeObj result
       where
+        --Check for ambiguity in the passed on goals.
         (wasAmbig, ambList, theChoices) =
           case resolveAmbig goals of
             Right _ -> (False, JSNull,"")
@@ -40,11 +41,11 @@ jsonMain jsinput =
         holding   = case valFromObj "holding" jsinput of
           Ok id   -> id
           Error _ -> ""
-
         hold      = ok (valFromObj "hold"      jsinput) :: Id
 
-        prevGoals = fmap read amb
+        prevGoals = fmap read amb --The goals from the previous utterance
 
+        --A new list of goals if clarified, otherwise return the same input
         goals
           | clarified = case prevGoals of
             Error _ -> error "!"
@@ -62,7 +63,8 @@ jsonMain jsinput =
             []        -> error "Fucking interpreter couldn't find a goal!"
             ([]:g:gs) -> solve world hold holding g :: Plan
             (g:gs)    -> solve world hold holding g :: Plan
-             
+
+        --Check user input and signal if True if valid
         clarified = case prevGoals of
             Error _ -> False
             Ok c -> case resolveAmbig c of
@@ -70,14 +72,16 @@ jsonMain jsinput =
                 Nothing -> False
                 Just x  -> True
               Right b -> True
-    
+
+        --Output choices, OK if no more ambs. or bad input msg.
         output
           | not clarified = "Ehh, why not trying to chose something that exist? Restarting."
           | clarified && wasAmbig = case resolveAmbig goals of
               Right a -> error "EH WHAT?"
               Left b -> "There is some ambiguity left, " ++ drop 16 (theChoices)
           | otherwise = "Great!"
-      
+
+        --For the next state, indicate ambiguity or clarified.
         result =
             [("utterance", showJSON utterance),
              ("plan", if clarified && not wasAmbig then showJSON plan else JSNull),
@@ -86,7 +90,8 @@ jsonMain jsinput =
                else [("state", showJSON "")]
 
     _  -> makeObj result
-      where       
+      where
+        --Check for ambiguity
         (wasAmbig, ambList, theChoices) =
           case resolveAmbig goals of
             Right _ -> (False, JSNull,"")
@@ -108,7 +113,7 @@ jsonMain jsinput =
 
         goals = filter (/= []) [goal | tree <- trees, goal <- interpret world holding objects tree] :: [[PDDL]]
 
-        plan = --"kisi"
+        plan = 
           case goals of
             [] -> error "Fucking interpreter couldn't find a goal!"
             ([]:g:gs) -> solve world hold holding g :: Plan
@@ -117,7 +122,7 @@ jsonMain jsinput =
         output
           | null trees = "Parse error!"
           | null goals = "Interpretation error!"
-          | wasAmbig = case (length goals > 5) of
+          | wasAmbig = case (length goals > 5) of --In case user is VERY ambiguous
                             True -> "There are "
                                      ++ show (length goals)
                                      ++ " things you could mean, please be more specific!"
@@ -132,6 +137,8 @@ jsonMain jsinput =
                    ("output", showJSON output)
                   ] ++ if wasAmbig && ((length goals) <= 5) then [("state", ambList)] else [("state", showJSON "")]
 
+--Checking for validity of input with an given ambiguity
+--Anything besides 1-5 is ignored.        
 getChoice :: [String] -> Ambiguity -> Maybe Int
 getChoice [] _ = Nothing
 getChoice [[x]] (Ambiguity _ _ idlist)
@@ -142,19 +149,25 @@ getChoice [[x]] (Ambiguity _ _ idlist)
   | otherwise = Nothing
 getChoice _ _ = Nothing
 
+--Generate a new list of goals given a choice of object
+--We build the ambiguitys from scratch here to
+--skip it as an input
 filterChoice :: Int -> PDDLWorld -> PDDLWorld
 filterChoice x yss = 
   case resolveAmbig yss of
-    Right b -> [b]
+    Right b -> [b] --Should not happen
     Left  (Ambiguity atype id list) ->
-      fltrGl (atype, list !! (x-1)) yss
+      fltrGl (atype, list !! (x-1)) yss --Pick out the element from the list
 
+--filter out source and destination ambs.
+--Inlined function
 fltrGl (t, i2) = filter flt
   where
     flt = case t of
       Source -> elemBy (\(PDDL _ id1 id2) -> i2 == id1)
       _      -> elemBy (\(PDDL _ id1 id2) -> i2 == id2)
 
+--helper funtion for special predficate filtering
 elemBy :: (a -> Bool) -> [a] -> Bool
 elemBy f (x:xs) | f x       = True
                 | otherwise = elemBy f xs
